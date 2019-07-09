e4688bffac3374cd6e9aea363a7fd8c58aa481d1
e01 <912744609@qq.com>
2019-07-09 17:38:48 +0800

    添加无限树状对象查找方法

--- vendor/esoft/sysFunc/Array.js
+++ vendor/esoft/sysFunc/Array.js
@@ -35,6 +35,43 @@ global.array2value = function(array,key1,value,key2,all) {
 }
 
 /**
+ * 方法根据key1=>value在array中查找相对的对象，并返回对象(中key2的值),或返回符合条件的所有对象
+ * array array 被查找的多个对象的数组
+ * key1 用于匹配的键
+ * value 用于匹配的键对应的值
+ * key2 string  null | key2  null 返回匹配对象， key2 返回匹配对象中 key2 的值
+ * all boolean true 返回所有匹配的集合，false 返回第一次匹配               
+ */
+global.treeValue = function(array,key1,value,key2,all) {
+	array = array || [];
+	all = all || false;
+    if(!array.length) return '';
+    if(typeof key2 == 'boolean') {
+        all = key2;
+        key2 = undefined;
+    }
+	var temObj = [];
+	for(var i in array){
+		var item = array[i];
+		for(var k in item){
+			if(item[k].constructor.name == 'Array'){
+				var values = treeValue(item[k],key1,value,key2,all);
+			}else{
+				if((k == key1) && (item[k] == value) && !all){
+					temObj = key2 ? item[key2] : item;
+					break;
+				}
+        		if((k == key1) && (item[k] == value) && all){
+					key2 ? temObj.push(item[key2]) : temObj.push(item);
+				} 
+			}
+		}
+	}
+    
+	return temObj.length ? temObj : '';
+}
+
+/**
  * 根据对象中的pid 和 id 关系，构造树状结构
  * 要求，根据 pid , id 进行升序排列
  * 必填参数 data: 被构造的原对象
